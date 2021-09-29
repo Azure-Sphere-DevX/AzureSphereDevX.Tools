093bac4f5aa59dcb50aaf0a73ea4cf45be07a564
Dave Glover <dglover@microsoft.com>
2021-09-30 09:04:25 +1000

    Create azsphere_config.cmake
    added auto_generate_azsphere_config cmake function

---
+++ cmake/azsphere_config.cmake
@@ -0,0 +1,33 @@
+# Auto generate azsphere_configure_tools and azsphere_configure_api
+function(auto_generate_azsphere_config)
+    # Generate the Azure Sphere tools version from the azsphere show-version SDK tool
+    execute_process(COMMAND azsphere show-version OUTPUT_VARIABLE SDK_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+    message(STATUS "Azure Sphere SDK Version: ${SDK_VERSION}")
+    azsphere_configure_tools(TOOLS_REVISION ${SDK_VERSION})
+
+    if (LINUX)
+        FILE(GLOB children "/opt/azurespheresdk/Sysroots")
+    else()
+        # Generate the Azure Sphere latest sysroot number
+        FILE(GLOB children "C:/Program Files (x86)/Microsoft Azure Sphere SDK/Sysroots/*")
+    endif()
+
+    list(LENGTH children len)
+
+    if(len EQUAL 0)
+        message(FATAL_ERROR "No Sysroots found in the default location. Check you have installed the Azure Sphere SDK")
+    endif()
+
+    list(SORT children)
+    list(GET children 0 child)
+    string(REPLACE "/" " " child ${child})
+    separate_arguments(child)
+
+    list(LENGTH child len)
+    MATH(EXPR len "${len}-1")
+    list(GET child ${len} sysroot)
+
+    message(STATUS "latest sysroot: ${sysroot}")
+    azsphere_configure_api(TARGET_API_SET ${sysroot})    
+
+endfunction()
